dbba7c4d9e3df01d44dbcd04094e1c946bdfe3ac
Ivan <rihar3681@mail.ru>
2024-05-18 16:43:09 +0300

    Created excellent game

--- main.py
+++ main.py
@@ -1,23 +1,51 @@
 import telebot
 
 from src.constants import *
+from src.game_realisation import *
 
 bot = telebot.TeleBot(token)
+
+word_dict = ["", 0]
+
 
 @bot.message_handler(commands=['start'])
 def main(message):
     bot.send_message(message.chat.id, start_text)
-    bot.send_message(message.chat.id, "You have written " + message.text)
+    word_dict[0] = ""
+    word_dict[1] = 0
+
 
 @bot.message_handler(commands=['help'])
 def main(message):
     bot.send_message(message.chat.id, help_text)
-
-@bot.message_handler(commands=['button'])
-def button_message(message):
-    markup=types.ReplyKeyboardMarkup(resize_keyboard=True)
-    item1=types.KeyboardButton("Начать игру!")
-    markup.add(item1)		
+    word_dict[0] = ""
+    word_dict[1] = 0
 
 
-bot.polling(none_stop = True)
+@bot.message_handler()
+def game_message(message):
+    if word_dict[0] == "":
+        if (len(message.text) >= 2 or
+                message.text < '3' or
+                message.text > '9'):
+            bot.send_message(message.chat.id,
+                             'Введите корректную длину слова (от 3 до 9)')
+        else:
+            word_dict[0] = random_word(int(message.text))
+            bot.send_message(message.chat.id,
+                             'Вы выбрали длину слова ' + message.text + '. Можете начинать игру)')
+    else:
+        result = get_match(message.text, word_dict[0])
+        if result[0] == 'П':
+            bot.send_message(message.chat.id,
+                             result + '. Вам потребовалось ' + str(word_dict[1] + 1) + ' попыток.')
+            bot.send_message(message.chat.id, 'Для того, чтобы начать новую игру, введите длину нового слова)')
+            word_dict[0] = ""
+            word_dict[1] = 0
+        else:
+            bot.send_message(message.chat.id, result)
+        if result[0] == 'Б':
+            word_dict[1] += 1
+
+
+bot.infinity_polling()
--- src/game_realisation.py
+++ src/game_realisation.py
@@ -1,18 +1,20 @@
 import random
 
+
 def random_word(digit_number):
     result = ''
     for i in range(digit_number):
         result += str(random.randrange(10))
     return result
 
+
 def get_match(user_word, game_word):
     if len(user_word) != len(game_word):
-        print("Слова разной длины")
-        return "diflen"
+        return "Слова разной длины"
+    if not user_word.isnumeric():
+        return "Слово должно состоять только из цифр"
     if user_word == game_word:
-        print("Победа! Было загадно число", game_word)
-        return "ok"
+        return "Победа! Было загадно число " + game_word
     bull_number = [0] * 10
     bull_sum = 0
     for i in range(len(game_word)):
@@ -21,15 +23,8 @@ def get_match(user_word, game_word):
     cow_number = [0] * 10
     cow_sum = 0
     for i in range(10):
-        cow_number[i] = min(user_word.count(chr(i+ord('0'))), game_word.count(chr(i+ord('0'))))
+        cow_number[i] = min(user_word.count(chr(i + ord('0'))), game_word.count(chr(i + ord('0'))))
         cow_number[i] -= bull_number[i]
         bull_sum += bull_number[i]
         cow_sum += cow_number[i]
-    print('Найдено', bull_sum, 'быков и', cow_sum, 'коров. Попробуйте ещё раз!')
-    return 'next'
-
-
-new_game = random_word(5)
-user_word = input()
-while get_match(user_word, new_game) != 'ok':
-    user_word = input()
+    return 'Быков: ' + str(bull_sum) + '. Коров: ' + str(cow_sum) + '. Попробуйте ещё раз!'
